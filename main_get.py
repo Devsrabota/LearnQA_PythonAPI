3aa6ecb267d070a9d51a9a40522dfc69e984227b
Дмитрий Шалаев <rabotadevs@gmail.com>
2022-03-24 11:38:11 +0300

    Start_1

--- main_get.py
+++ main_get.py
@@ -1,5 +1,7 @@
 from urllib import response
+# Импортируем запрос
 import requests
-
+# Создаем запрос 
 response = requests.get("https://playground.learnqa.ru/api/get_text")
+# Выводим ответ
 print(response.text)
