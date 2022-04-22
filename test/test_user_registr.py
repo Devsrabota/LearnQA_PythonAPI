1bebcc89028a82615bc475c7a75ba5eda300d0ef
Дмитрий Шалаев <rabotadevs@gmail.com>
2022-04-22 19:20:21 +0300

    new game

--- test/test_user_registr.py
+++ test/test_user_registr.py
@@ -5,9 +5,9 @@ from lib.base_case import BaseCase
 #
 class TestUserRegister(BaseCase):
       def test_create_user_with_existing_email(self):
-          str1 = 'vinkotov'
-          str2 = '@'
-          str3 = 'examle.com'
+          str1 = str('vinkotov')
+          str2 = str('@')
+          str3 = str('example.com')
           email_all = str1 + str2 + str3
           data = {
                'password': '123',
@@ -22,12 +22,12 @@ class TestUserRegister(BaseCase):
           print(response.content)
 
           email_st = len(email_all)
-          if email_st == 1:
-               assert True, f"error '{email_all}' 1 simvol"
+          if   email_st == 1:
+               assert False, f"error '{email_all}' 1 simvol"
           elif email_st == 255:
-               assert True, f"error '{email_all}' 255 simvol"
+               assert False, f"error '{email_all}' 255 simvol"
 
-          assert response.status_code != 400, f"Status Code bad"
+          assert response.status_code == 400, f"Status Code bad"
 
           assert len(str2) != 0, f"error '{email_all}' not @"
           assert len(str1) != 0, f"error not name@'{str3}'"
